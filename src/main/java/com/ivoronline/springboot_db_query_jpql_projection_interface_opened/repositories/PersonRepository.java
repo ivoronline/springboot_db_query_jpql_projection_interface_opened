76f7fd3d317d873a66e12721df1e3e1822bde681
ivoronline <ivoronline@gmail.com>
2021-11-26 16:11:24 +0100

    Commit

--- src/main/java/com/ivoronline/springboot_db_query_jpql_projection_interface_opened/repositories/PersonRepository.java
+++ src/main/java/com/ivoronline/springboot_db_query_jpql_projection_interface_opened/repositories/PersonRepository.java
@@ -1,17 +1,17 @@
 package com.ivoronline.springboot_db_query_jpql_projection_interface_opened.repositories;
 
 import com.ivoronline.springboot_db_query_jpql_projection_interface_opened.entities.Person;
-import com.ivoronline.springboot_db_query_jpql_projection_interface_opened.entities.PersonView;
+import com.ivoronline.springboot_db_query_jpql_projection_interface_opened.entities.PersonProjection;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface PersonRepository extends CrudRepository<Person, Integer> {
 
   //=======================================================================================
-  // RETURN PERSON VIEW
+  // RETURN PERSON PROJECTION
   //=======================================================================================
-  @Query(value = "SELECT person.name AS name, person.age AS age FROM Person person WHERE person.name='John'")
-  PersonView returnPersonView();
+  @Query(value = "SELECT person FROM Person person WHERE person.name='John'")
+  PersonProjection returnPersonProjection();
 
 }
 
